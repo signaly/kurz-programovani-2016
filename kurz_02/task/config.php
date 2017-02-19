3adbdfe5101fa95c3c62cac9bedf5bca3c20b9ea
Jan Skrasek <hrach.cz@gmail.com>
2017-02-19 11:41:53 +0100

    kurz02: default connection is to localhost

--- kurz_02/task/config.php
+++ kurz_02/task/config.php
@@ -4,14 +4,13 @@ const DATA_FILE = __DIR__ . '/data.dat';
 const MIN_USERNAME_LENGTH = 3;
 const MIN_PASSWORD_LENGTH = 5;
 
-require_once __DIR__ .'/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
-use Tracy\Debugger;
-Debugger::enable();
+Tracy\Debugger::enable();
 
 $dbConnection = new Nextras\Dbal\Connection([
 	'driver'   => 'mysqli',
-	'host'     => '192.168.56.10',
+	'host'     => 'localhost',
 	'username' => 'root',
 	'password' => '',
 	'database' => 'test',
